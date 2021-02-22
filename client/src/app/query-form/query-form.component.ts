22d53878c2387f0561fb88f069d9021fdaec7b14
amiradzim <amir.adzim@gmail.com>
2021-02-22 11:56:03 +0800

    Visual enhancement on the four tabs

--- client/src/app/query-form/query-form.component.ts
+++ client/src/app/query-form/query-form.component.ts
@@ -5,6 +5,7 @@ import { EntriesService } from '../_services/entries.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import * as XLSX from "xlsx";
 
 @Component({
   selector: 'app-query-form',
@@ -14,6 +15,7 @@ import { MatSort } from '@angular/material/sort';
 
 export class QueryFormComponent implements OnInit {
   isLinear = false;
+  fileName = 'consolidated-mto.xlsx'
   toSelectGroup: FormGroup;
   toGroupByGroup: FormGroup;
   toSumGroup: FormGroup;
@@ -72,7 +74,7 @@ export class QueryFormComponent implements OnInit {
 
     })
   }
-  exportcsv() {
+  /*exportcsv() {
     var html = document.getElementById('entries-table').outerHTML;
     this.export_table_to_csv(html, "table.csv");
   }
@@ -117,6 +119,19 @@ export class QueryFormComponent implements OnInit {
 
     // Download CSV
     this.download_csv(csv.join("\n"), filename);
+  }*/
+
+  exportexcel(): void {
+    /* table id is passed over here */
+    let element = document.getElementById('entries-table');
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
   }
 
   onRowClicked(row) {
